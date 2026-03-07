6638ec818cd272c37178ecd2fd669c74d68f0881
Curro Portillo <211curroportillo@gmail.com>
2026-03-07 23:31:08 +0100

    Ejercicios 1-3

--- src/Introducion/App.java
+++
@@ -1,9 +0,0 @@
-package Introducion;
-
-import java.io.BufferedReader;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(null);
-    }
-}
